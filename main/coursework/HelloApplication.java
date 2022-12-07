1f661f0b0778d2a417135f03da5ed1f0ff20e40a
BaderShalata <99408220+BaderShalata@users.noreply.github.com>
2022-12-07 15:20:29 +0000

    up to date menu

--- main/coursework/HelloApplication.java
+++ main/coursework/HelloApplication.java
@@ -1,31 +1,45 @@
-package coursework;
-
+package com.example.newproject;
+/**
+ @Author Bader Shalata - 2221254
+ @Date Sep-Dec
+ */
+/** imports */
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+/** this class is the main launching class for the menu  */
+public class HelloApplication extends Application
+{
+   /** @throws IOException if file does not exist where it's assigned.
+    *  @param - Stage parameter */
+   @FXML
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+   {
         Parent root = FXMLLoader.load(getClass().getResource("actionMenu.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
-        stage.setOnCloseRequest(event ->
+        stage.setOnCloseRequest
+                (event ->
         {
             event.consume();
             logout(stage);
-        });
+        }       );
     }
-
+/** this logout method verifies that the user wants to exit for sure.
+ * @param - Stage parameter. */
     public void logout(Stage stage)
     {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -42,6 +56,7 @@ public class HelloApplication extends Application {
 
 
     }
+    /** Main class that launches the app. */
     public static void main(String[] args) {
         launch();
     }
--- main/coursework/HelloController.java
+++ main/coursework/HelloController.java
@@ -1,5 +1,5 @@
-package coursework;
-
+package com.example.newproject;
+/** imports */
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,14 +8,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 
-public class HelloController {
+/** this class contains all the methods the behaving methods for the menu class.
+ * @Bader Shalata - 2221254
+ * Date - Sep-Dec */
+public class HelloController extends HelloApplication{
     private Parent root;
     private Stage stage;
     private Scene scene;
@@ -23,53 +23,109 @@ public class HelloController {
     private Button exitButton;
     @FXML
     private AnchorPane scenePane;
+    @FXML
+    private TextField Player;
+    @FXML
+    private TextField dayMessage;
+
+
+    /**
+     * playerName method handles the input for player name and stores it in a variable.
+     */
+    @FXML
+    private TextField textarea123;
+    public void playerName()
+    {
+        String userInput = Player.getText();
+        textarea123.setText(userInput);
+    }
+
+
+    /**
+     * this logout method verifies that the user wants to exit for sure.
+     *
+     * @param - ActionEvent event
+     */
     public void logout(ActionEvent event)
     {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
 
-            alert.setTitle("Logout");
-            alert.setHeaderText("You're about to logout!");
-            alert.setContentText("Do you want to save before exiting? ");
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting? ");
 
-            if(alert.showAndWait().get() == ButtonType.OK)
-            {
-                stage = (Stage) scenePane.getScene().getWindow();
-                System.out.println("GOOD JOB");
-                stage.close();
-            }
+        if (alert.showAndWait().get() == ButtonType.OK)
+        {
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("GOOD JOB");
+            stage.close();
+        }
     }
+
+    /**
+     * this method allows us to switch scenes.
+     *
+     * @param - ActionEvent event
+     * @throws IOException if file does not exist where it's assigned.
+     */
     public void switchToScene1(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("actionMenu.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * this method allows us to switch back.
+     *
+     * @param - ActionEvent event
+     * @throws IOException if file does not exist where it's assigned.
+     */
     public void switchToScene2(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("scoreBoard.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * this method allows us to switch to level menu.
+     *
+     * @param - ActionEvent event
+     * @throws IOException if file does not exist where it's assigned.
+     */
     public void switchToLevel(ActionEvent event) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("Levels.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    @FXML
-    private TextField dayMessage;
+
+
+
+    /**
+     * method that takes message of the day input and outputs it.
+     *
+     * @param - (message) ADD MESSAGE OF THE DAY.
+     * @throws IOException if file does not exist where it's assigned.
+     */
     public void textfieldDisplay() throws IOException
     {
         String message;
         message = "Hello";
         dayMessage.setText(message);
     }
+
+    /**
+     * method that removes text display
+     * @throws IOException if file does not exist where it's assigned.
+     */
     public void textfieldRemoveDisplay() throws IOException
     {
         dayMessage.setText("");
