15c375a611c467f7a565d895337ecd90aa618f6f
Alex <alexgrendell@hotmail.com>
2022-12-11 12:52:29 +0000

    Some Javadoc

--- main/coursework/LevelSelection.java
+++ main/coursework/LevelSelection.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 /**
  * Adds the scores of each level to a file
- * 
  * @author Alex Rendell 211331
  */
 
@@ -15,6 +14,12 @@ public class LevelSelection {
 	LevelControl control = new LevelControl();
 	private static int levelNumberStorage;
 
+	/**
+	 * Creates a new file or adds the score to previous level file
+	 * @param level Current player level
+	 * @param user Username of the player
+	 * @param score score achieved when level is complete
+	 */
 	static public void score(int level, String user, int score)
 			throws IOException {
 		try {
@@ -33,26 +38,40 @@ public class LevelSelection {
 			}
 		}
 	}
-	// creates a new file
+
+	/**
+	 * Creates a new file and adds user score to it
+	 * @param level Current player level
+	 * @param user Username of the player
+	 * @param score score achieved when level is complete
+	 */
 	public static void newLevelFile(int level, String user, int score)
 			throws IOException {
 		File f = new File("level" + level + "_score.txt");
 		f.createNewFile();
 		addScore(level, user, score);
 	}
-	// adds score and name to file
-	static public String addScore(int level, String user, int score) {
+
+	/**
+	 * Adds username and score to the level file
+	 * @param level Current player level
+	 * @param user Username of the player
+	 * @param score score achieved when level is complete
+	 */
+	static public void addScore(int level, String user, int score) {
 		try {
 			PrintWriter out = new PrintWriter(new FileWriter(
 					"scores/" + "level" + level + "_score.txt", true));
-			out.write(user + "»" + score + "\n");
+			out.write(user + "ï¿½" + score + "\n");
 			out.close();
 		} catch (IOException I) {
 			System.out.println("addScore catch");
 		}
-		return null;
 	}
-
+	/**
+	 * Reads the score inside the level .txt file
+	 * @param level The level scoreboard wanting to be displayed
+	 */
 	static public void readScore(int level) {
 
 		File myObj = new File("scores/" + "level" + level + "_score.txt");
@@ -71,6 +90,10 @@ public class LevelSelection {
 		}
 	}
 
+	/**
+	 * Sorts all user score into an array list
+	 * @param level The level wanting to be sorted
+	 */
 	static public ArrayList<ScoreEntry> readScoreList(int level) {
 		ArrayList<ScoreEntry> scoreList = new ArrayList<ScoreEntry>();
 		try {
@@ -79,7 +102,7 @@ public class LevelSelection {
 				Scanner myReader = new Scanner(myObj);
 				while (myReader.hasNextLine()) {
 					String data = myReader.nextLine();
-					String[] nameScore = data.split("»");
+					String[] nameScore = data.split("ï¿½");
 					ScoreEntry thisEntry = new ScoreEntry();
 					thisEntry.setProfileName(nameScore[0]);
 					thisEntry.setScore(Integer.parseInt(nameScore[1]));
@@ -96,6 +119,7 @@ public class LevelSelection {
 		return scoreList;
 	}
 
+
 	public static void main(String args[]) throws IOException {
 
 		LevelSelection l = new LevelSelection();
@@ -107,9 +131,19 @@ public class LevelSelection {
 		l.readScore(1);
 		l.readScore(2);
 	}
+
+	/**
+	 * Gets the level in storage
+	 * @return return the level in storage
+	 */
 	public static int getLevelNumberStorage() {
 		return levelNumberStorage;
 	}
+
+	/**
+	 * Set the current level in storage
+	 * @param levelNumberStorage New number in storage
+	 */
 	public static void setLevelNumberStorage(int levelNumberStorage) {
 		LevelSelection.levelNumberStorage = levelNumberStorage;
 	}
