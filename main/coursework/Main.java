957c13d9d0d468b27d30865e92e926063ba8cd6a
EnzoTobias <98045922+EnzoTobias@users.noreply.github.com>
2022-12-11 19:48:37 +0000

    more changes

--- main/coursework/Main.java
+++ main/coursework/Main.java
@@ -48,13 +48,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
- * Sample application that demonstrates the use of JavaFX Canvas for a Game.
- * This class is intentionally not structured very well. This is just a starting
- * point to show how to draw an image on a canvas, respond to arrow key presses,
- * use a tick method that is called periodically, and use drag and drop.
- * 
- * Do not build the whole application in one file. This file should probably
- * remain very small.
+ * General application that runs and integrates the game's features.
  *
  * @author Liam O'Reilly, Freddie, Enzo Tobias 2117781, Bader Shalata
  */
